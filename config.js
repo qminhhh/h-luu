e29a1cb395d88f41495b9409fde1da44d57f9ae6
qminhhh <87742898+qminhhh@users.noreply.github.com>
2021-07-21 10:22:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,6 @@ const CONFIG = {
     mess: 'bíc gòi nha nghịn mà con ngại =))',
     messDesc: 'Tối nay triển lun ha.',
     btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100058929501920'
 }
  
